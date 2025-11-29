38487bd9fcc6dde6592ce7928aca09db703b1872
raissa <bombokoraissa@gmail.com>
2025-11-29 13:53:42 +0100

    feat(nuxt.config.s): implemented the production backend

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -25,7 +25,8 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || "http://127.0.0.1:3333/api", // URL de l'API backend
+      // apiBase: process.env.NUXT_PUBLIC_API_BASE || "http://127.0.0.1:3333/api", // URL de l'API backend
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || "https://portfolioraissa.fly.dev",
     },
   },
 
