30e0944800492d77334dcfcf037828b325b68c3b
raissa <bombokoraissa@gmail.com>
2026-05-18 12:28:34 +0100

    Fix: remove unused beforeSave import

--- backend/app/models/user.ts
+++ backend/app/models/user.ts
@@ -1,7 +1,7 @@
 import { DateTime } from 'luxon'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, column, hasMany, beforeSave } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
 import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
 import Project from './project.js'
